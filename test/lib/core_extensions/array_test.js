28a7ef0a2673d599830d199e28e5b133659a051b
Alexey D <ord.alwo@gmail.com>
2020-02-07 23:36:51 +0300

    Added core_extensions: array

---
+++ test/lib/core_extensions/array_test.js
@@ -0,0 +1,20 @@
+import test from "tape";
+import { capitalize } from "core_extensions/string";
+import { joinBy, ensureIsArray } from "core_extensions/array";
+
+test("core extensions: array", function(t) {
+  t.test("joinBy", function(t) {
+    t.equal(joinBy([], capitalize), "", "returns empty string for empty array");
+    t.equal(joinBy(["user"], capitalize), "user", "returns single element from array unmodified");
+    t.equal(joinBy(["api", "user", "path"], capitalize), "apiUserPath", "applies function to 1..n elements of the array before joining it");
+
+    t.end();
+  });
+
+  t.test("ensureIsArray", function(t) {
+    t.same(ensureIsArray([1]), [1], "returns arrays unmodified");
+    t.same(ensureIsArray(1), [1], "wraps any other values into array");
+
+    t.end();
+  });
+});
