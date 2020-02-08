702fe035b891feb837a933ec9b3bc536b0ce0f81
Alexey D <ord.alwo@gmail.com>
2020-02-08 17:23:04 +0300

    Added camelize()

--- test/lib/core_extensions/string_test.js
+++ test/lib/core_extensions/string_test.js
@@ -1,5 +1,5 @@
 import test from "enhanced-tape";
-import { capitalize } from "core_extensions/string";
+import { capitalize, camelize } from "core_extensions/string";
 
 test("core extenstions: String", function(t) {
   t.test("capitalize()", function(t) {
@@ -10,5 +10,11 @@ test("core extenstions: String", function(t) {
     });
   });
 
-  t.end();
+  t.test("camelize()", function(t) {
+    t.equal(camelize("fooBar"), "fooBar", "returns camelized strings unchanged");
+    t.equal(camelize("foo_bar_test"), "fooBarTest", "camelizes underscore_notation");
+    t.equal(camelize("foo-bar-test"), "fooBarTest", "camelizes dashed-notation");
+
+    t.end();
+  });
 });
